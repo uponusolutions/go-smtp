f8d725c79d6c5d84392a2ae98e1e4fc246c9dcef
Marcel Link <marcel.link@t-online.de>
2025-06-05 15:56:11 +0200

    test: lorem ipsum dotwriter

--- internal/textsmtp/writer_test.go
+++ internal/textsmtp/writer_test.go
@@ -8,14 +8,107 @@ import (
 	"bufio"
 	"bytes"
 	"crypto/rand"
+	"embed"
 	"io"
 	legacy "net/textproto"
+	"path/filepath"
 	"testing"
 
+	"github.com/stretchr/testify/require"
 	"github.com/uponusolutions/go-smtp/internal/textsmtp"
 )
 
+func getAllFilenames(fs *embed.FS, path string) (out []string, err error) {
+	if len(path) == 0 {
+		path = "."
+	}
+	entries, err := fs.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+	for _, entry := range entries {
+		fp := filepath.Join(path, entry.Name())
+		if entry.IsDir() {
+			res, err := getAllFilenames(fs, fp)
+			if err != nil {
+				return nil, err
+			}
+			out = append(out, res...)
+			continue
+		}
+		out = append(out, fp)
+	}
+	return
+}
+
+func chunkSlice(slice []byte, chunkSize int) [][]byte {
+	var chunks [][]byte
+	for {
+		if len(slice) == 0 {
+			break
+		}
+
+		// necessary check to avoid slicing beyond
+		// slice capacity
+		if len(slice) < chunkSize {
+			chunkSize = len(slice)
+		}
+
+		chunks = append(chunks, slice[0:chunkSize])
+		slice = slice[chunkSize:]
+	}
+
+	return chunks
+}
+
+func CheckAgainstOld(t *testing.T, b []byte) {
+	var buf bytes.Buffer
+	var err error
+
+	f := legacy.NewWriter(bufio.NewWriter(&buf)).DotWriter()
+	_, err = f.Write(b)
+	require.NoError(t, err)
+	require.NoError(t, f.Close())
+
+	size := 1
+
+	for size < 4048 && len(b) >= size {
+		bsplitted := chunkSlice(b, size)
+
+		var buf1 bytes.Buffer
+		f = legacy.NewWriter(bufio.NewWriter(&buf1)).DotWriter()
+
+		for _, b := range bsplitted {
+			_, err = f.Write(b)
+			require.NoError(t, err)
+		}
+
+		require.NoError(t, f.Close())
+
+		require.Equal(t, buf, buf1)
+
+		size++
+	}
+
+}
+
+//go:embed testdata/*
+var embedFS embed.FS
+
 func TestDotWriter(t *testing.T) {
+	t.Run("Testdata", func(t *testing.T) {
+
+		files, err := getAllFilenames(&embedFS, "testdata")
+		require.NoError(t, err)
+
+		for _, file := range files {
+			dat, err := embedFS.ReadFile(file)
+			require.NoError(t, err)
+			CheckAgainstOld(t, []byte(dat))
+		}
+
+	})
+
 	t.Run("Encode", func(t *testing.T) {
 		var buf bytes.Buffer
 		d := textsmtp.NewDotWriter(bufio.NewWriter(&buf))
