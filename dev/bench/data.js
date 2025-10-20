81ad0cb85ca1cafe21e734554da67b8798d1358b
github-action-benchmark <github@users.noreply.github.com>
2025-10-20 19:33:40 +0000

    add Benchmark (go) benchmark result for 8b4026755ed69b048d3ce48c3e36ebb8abc24c28

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1760988129573,
+  "lastUpdate": 1760988820112,
   "repoUrl": "https://github.com/uponusolutions/go-smtp",
   "entries": {
     "Benchmark": [
@@ -96,6 +96,102 @@ window.BENCHMARK_DATA = {
             "value": 26.71,
             "unit": "MB/s",
             "extra": "8404 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "marcel.link@uponu.com",
+            "name": "Marcel Link",
+            "username": "ml1nk"
+          },
+          "committer": {
+            "email": "marcel.link@uponu.com",
+            "name": "Marcel Link",
+            "username": "ml1nk"
+          },
+          "distinct": true,
+          "id": "8b4026755ed69b048d3ce48c3e36ebb8abc24c28",
+          "message": "fix: typo",
+          "timestamp": "2025-10-20T21:33:17+02:00",
+          "tree_id": "4c15dc4256c4a1ad2a562a9d4dfa844020844b12",
+          "url": "https://github.com/uponusolutions/go-smtp/commit/8b4026755ed69b048d3ce48c3e36ebb8abc24c28"
+        },
+        "date": 1760988819724,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkSmallWithChunking",
+            "value": 393544,
+            "unit": "ns/op\t   8.98 MB/s",
+            "extra": "2940 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSmallWithChunking - ns/op",
+            "value": 393544,
+            "unit": "ns/op",
+            "extra": "2940 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSmallWithChunking - MB/s",
+            "value": 8.98,
+            "unit": "MB/s",
+            "extra": "2940 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSmallWithChunkingSameConnection",
+            "value": 132671,
+            "unit": "ns/op\t  26.64 MB/s",
+            "extra": "8528 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSmallWithChunkingSameConnection - ns/op",
+            "value": 132671,
+            "unit": "ns/op",
+            "extra": "8528 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSmallWithChunkingSameConnection - MB/s",
+            "value": 26.64,
+            "unit": "MB/s",
+            "extra": "8528 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSmallWithoutChunking",
+            "value": 393437,
+            "unit": "ns/op\t   8.98 MB/s",
+            "extra": "3013 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSmallWithoutChunking - ns/op",
+            "value": 393437,
+            "unit": "ns/op",
+            "extra": "3013 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSmallWithoutChunking - MB/s",
+            "value": 8.98,
+            "unit": "MB/s",
+            "extra": "3013 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSmallWithoutChunkingSameConnection",
+            "value": 132687,
+            "unit": "ns/op\t  26.63 MB/s",
+            "extra": "8865 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSmallWithoutChunkingSameConnection - ns/op",
+            "value": 132687,
+            "unit": "ns/op",
+            "extra": "8865 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSmallWithoutChunkingSameConnection - MB/s",
+            "value": 26.63,
+            "unit": "MB/s",
+            "extra": "8865 times\n4 procs"
           }
         ]
       }
