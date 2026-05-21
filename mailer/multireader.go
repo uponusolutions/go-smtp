94cd08e7eb73ceeb65492abc2a3466d91cf7fe21
Marcel Link <marcel.link@uponu.com>
2026-05-21 18:21:14 +0200

    feat: improved multi reader

--- mailer/multireader.go
+++ mailer/multireader.go
@@ -1,16 +1,72 @@
 package mailer
 
-import "io"
+import (
+	"io"
+)
 
-// ReaderLen extends io.Reader with Len.
-type ReaderLen interface {
+// ReaderWriteToLen extends io.Reader with Len and WriteTo.
+type ReaderWriteToLen interface {
 	io.Reader
+	io.WriterTo
 	Len() int
 }
 
+// Modified from https://github.com/golang/go/blob/master/src/io/multi_test.go
 type multiReader struct {
-	io.Reader
-	readers []ReaderLen
+	readers []ReaderWriteToLen
+}
+
+type eofReader struct{}
+
+func (eofReader) Read([]byte) (int, error) {
+	return 0, io.EOF
+}
+
+func (eofReader) Len() int {
+	return 0
+}
+
+func (eofReader) WriteTo(_ io.Writer) (sum int64, err error) {
+	return 0, nil
+}
+
+func (mr *multiReader) Read(p []byte) (n int, err error) {
+	for len(mr.readers) > 0 {
+		// Optimization to flatten nested multiReaders (Issue 13558).
+		if len(mr.readers) == 1 {
+			if r, ok := mr.readers[0].(*multiReader); ok {
+				mr.readers = r.readers
+				continue
+			}
+		}
+		n, err = mr.readers[0].Read(p)
+		if err == io.EOF {
+			// Use eofReader instead of nil to avoid nil panic
+			// after performing flatten (Issue 18232).
+			mr.readers[0] = eofReader{} // permit earlier GC
+			mr.readers = mr.readers[1:]
+		}
+		if n > 0 || err != io.EOF {
+			if err == io.EOF && len(mr.readers) > 0 {
+				// Don't return EOF yet. More readers remain.
+				err = nil
+			}
+			return n, err
+		}
+	}
+	return 0, io.EOF
+}
+
+func (mr *multiReader) WriteTo(w io.Writer) (sum int64, err error) {
+	n := int64(0)
+	for _, r := range mr.readers {
+		rn, err := r.WriteTo(w)
+		n += rn
+		if err != nil {
+			return n, err
+		}
+	}
+	return n, nil
 }
 
 func (mr *multiReader) Len() int {
@@ -21,17 +77,29 @@ func (mr *multiReader) Len() int {
 	return length
 }
 
-// MultiReader is essentially io.MultiReader but requires and provides Len.
-func MultiReader(readers ...ReaderLen) ReaderLen {
-	ioReaders := make([]io.Reader, len(readers))
-	for i, r := range readers {
-		ioReaders[i] = r
-	}
+// MultiReaderFactory is a utility struct to ease MultiReader creation.
+type MultiReaderFactory struct {
+	readers []ReaderWriteToLen
+}
 
-	log := &multiReader{
-		Reader:  io.MultiReader(ioReaders...),
-		readers: readers,
-	}
+// Add adds the next readers to the factory.
+// The new [MultiReaderFactory] takes ownership of the Readers,
+func (mrf *MultiReaderFactory) Add(readers ...ReaderWriteToLen) {
+	mrf.readers = append(mrf.readers, readers...)
+}
 
-	return log
+// Create creates a MultiReader from all added readers.
+func (mrf *MultiReaderFactory) Create() ReaderWriteToLen {
+	reader := MultiReader(mrf.readers...)
+	mrf.readers = nil
+	return reader
+}
+
+// MultiReader returns a Reader that's the logical concatenation of
+// the provided input readers. They're read sequentially. Once all
+// inputs have returned EOF, Read will return EOF.  If any of the readers
+// return a non-nil, non-EOF error, Read will return that error.
+// The new [Reader] takes ownership of the Readers,
+func MultiReader(readers ...ReaderWriteToLen) ReaderWriteToLen {
+	return &multiReader{readers}
 }
--- mailer/multireader_test.go
+++ mailer/multireader_test.go
@@ -2,6 +2,8 @@ package mailer_test
 
 import (
 	"bytes"
+	"io"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -41,4 +43,78 @@ func TestClient_Multireader(t *testing.T) {
 	require.Equal(t, bytes, []byte{'d'})
 	require.NoError(t, err)
 	require.Equal(t, 0, res.Len())
+}
+
+// Test taken from https://github.com/golang/go/blob/master/src/io/multi_test.go
+func TestMultiReader(t *testing.T) {
+	var mr io.Reader
+	var buf []byte
+	nread := 0
+	withFooBar := func(tests func()) {
+		r1 := strings.NewReader("foo ")
+		r2 := strings.NewReader("")
+		r3 := strings.NewReader("bar")
+		mr = mailer.MultiReader(r1, r2, r3)
+		buf = make([]byte, 20)
+		tests()
+	}
+	expectRead := func(size int, expected string, eerr error) {
+		nread++
+		n, gerr := mr.Read(buf[0:size])
+		if n != len(expected) {
+			t.Errorf("#%d, expected %d bytes; got %d",
+				nread, len(expected), n)
+		}
+		got := string(buf[0:n])
+		if got != expected {
+			t.Errorf("#%d, expected %q; got %q",
+				nread, expected, got)
+		}
+		if gerr != eerr {
+			t.Errorf("#%d, expected error %v; got %v",
+				nread, eerr, gerr)
+		}
+		buf = buf[n:]
+	}
+	withFooBar(func() {
+		expectRead(2, "fo", nil)
+		expectRead(5, "o ", nil)
+		expectRead(5, "bar", nil)
+		expectRead(5, "", io.EOF)
+	})
+	withFooBar(func() {
+		expectRead(4, "foo ", nil)
+		expectRead(1, "b", nil)
+		expectRead(3, "ar", nil)
+		expectRead(1, "", io.EOF)
+	})
+	withFooBar(func() {
+		expectRead(5, "foo ", nil)
+	})
+}
+
+// Test taken from https://github.com/golang/go/blob/master/src/io/multi_test.go
+func TestMultiReaderAsWriterTo(t *testing.T) {
+	mr := mailer.MultiReader(
+		strings.NewReader("foo "),
+		mailer.MultiReader(
+			strings.NewReader(""),
+			strings.NewReader("bar"),
+		),
+	)
+	mrAsWriterTo, ok := mr.(io.WriterTo)
+	if !ok {
+		t.Fatal("expected cast to WriterTo to succeed")
+	}
+	sink := &strings.Builder{}
+	n, err := mrAsWriterTo.WriteTo(sink)
+	if err != nil {
+		t.Fatalf("expected no error; got %v", err)
+	}
+	if n != 7 {
+		t.Errorf("expected read 7 bytes; got %d", n)
+	}
+	if result := sink.String(); result != "foo bar" {
+		t.Errorf(`expected "foo bar"; got %q`, result)
+	}
 }
